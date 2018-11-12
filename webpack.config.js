adb787a7f03f36e3ee1e0548561ff019c89f7128
helingfeng <857190327@qq.com>
2018-11-12 10:19:54 +0800

    Chnaged: webpack vue  demo

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,23 @@
 const path = require('path');
 
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
 const config = {
-    entry: './src/demo1.js',
+    entry: './src/main.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js'
-    }
+    },
+    devServer: {
+        contentBase: './dist'
+    },
+    plugins: [
+        new CleanWebpackPlugin(['dist']),
+        new HtmlWebpackPlugin({
+            template: 'index.html',
+        })
+    ]
 };
 
 module.exports = config;
