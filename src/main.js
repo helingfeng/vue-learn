8a5bb1381a5e342e0f6764c9bef653ea813c0b14
helingfeng <857190327@qq.com>
2018-11-12 10:20:23 +0800

    Chnaged: webpack vue  demo

---
+++ src/main.js
@@ -0,0 +1,13 @@
+import Vue from 'vue';
+
+var app5 = new Vue({
+    el: '#app-5',
+    data: {
+      message: 'Hello Vue.js!'
+    },
+    methods: {
+      reverseMessage: function () {
+        this.message = this.message.split('').reverse().join('')
+      }
+    }
+});
