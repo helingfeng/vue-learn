efdcea94984416e70840e4543287f4cb61dcc8e9
helingfeng <857190327@qq.com>
2018-11-26 21:54:08 +0800

    Changed: Vue computed

--- src/main.js
+++ src/main.js
@@ -1,13 +1,33 @@
 import Vue from 'vue';
 
-var app5 = new Vue({
-    el: '#app-5',
-    data: {
-      message: 'Hello Vue.js!'
-    },
-    methods: {
-      reverseMessage: function () {
-        this.message = this.message.split('').reverse().join('')
-      }
+// var app5 = new Vue({
+//     el: '#app-5',
+//     data: {
+//       message: 'Hello Vue.js!'
+//     },
+    // methods: {
+    //   reverseMessage: function () {
+    //     this.message = this.message.split('').reverse().join('')
+    //   }
+    // }
+// });
+
+
+var vm = new Vue({
+  el: '#app-5',
+  data: {
+    message: 'Hello'
+  },
+  computed: {
+    // 计算属性的 getter
+    reversedMessage: function () {
+      // `this` 指向 vm 实例
+      return this.message.split('').reverse().join('')
     }
-});
+  },
+  methods: {
+    reverseMessageHandle: function () {
+      this.message = this.message.split('').reverse().join('')
+    }
+  }
+})
